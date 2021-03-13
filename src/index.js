23b4d5be274c9f37b26610ef73d02dfe3c4ee283
VPA273 <vpa273@gmail.com>
2021-03-13 20:07:28 +0300

    refactor: apply prettier

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@
 
 module.exports = function towelSort (matrix) {
   let arr = [];
-  for( let i=0 ; i < matrix.length ; i++ ) {
+  if (!matrix) return arr;
+  if (matrix.length ==0) return arr;
+  if (matrix == undefined) return arr;
+  for (item = 1; item < matrix.length ; item++ ) {
+          if( item % 2 ==1 ) { matrix[item].reverse() }
+      for( let i=0 ; i < matrix.length ; i++ ) {
     for( let p=0 ; p < matrix.length ; p++) {
-      arr.push( matrix[i][p] );
+      arr.push(matrix[i][p])
     }
-    } 
-   return arr;
+  }
+  return arr;
 }
